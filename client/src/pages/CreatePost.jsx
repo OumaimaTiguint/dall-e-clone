428eac031da462c43fd8c1b93974376ad7442085
OumaimaTiguint <oumaimatiguint@gmail.com>
2023-04-06 14:18:47 +0000

    changed to render links

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -26,7 +26,7 @@ const CreatePost = () => {
             setLoading(true);
 
             try {
-                const response = await fetch('http://localhost:8080/api/v1/post', {
+                const response = await fetch('https://dall-e-2oab.onrender.com/api/v1/post', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
@@ -54,7 +54,7 @@ const CreatePost = () => {
         if(form.prompt) {
             try {
                 setgeneratingImg(true);
-                const response = await fetch('http://localhost:8080/api/v1/dalle', {
+                const response = await fetch('https://dall-e-2oab.onrender.com/api/v1/dalle', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
